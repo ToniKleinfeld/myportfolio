6c8b85a3670e960ea3a7547125bf6413a6a3decd
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-12-11 15:36:06 +0100

    add keyframe for swich effect when change language

--- src/app/shared/components/header/nav/iconssection/iconssection.component.ts
+++ src/app/shared/components/header/nav/iconssection/iconssection.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-iconssection',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './iconssection.component.html',
   styleUrl: './iconssection.component.scss'
 })
 export class IconssectionComponent {
+  translate:boolean = true;
 
+  changeLanguage(){
+    if (this.translate == true) {
+      this.translate = false;
+    } else {
+      this.translate = true;
+    }
+  }
 }
