a5092d7d2fc45b39e6fa106e21d4748e8bb2eb77
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-12-12 15:43:50 +0100

    add greet component with typing effect name

--- src/app/big-pic-greet/big-pic-greet.component.ts
+++ src/app/big-pic-greet/big-pic-greet.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-pic-greet',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './big-pic-greet.component.html',
   styleUrl: './big-pic-greet.component.scss'
 })
-export class BigPicGreetComponent {
+export class BigPicGreetComponent implements OnInit{
 
+  typeAnimationh1:boolean = false;
+  tpyeAnimationh3:boolean = false;
+  hideh3:boolean = true;
+
+  ngOnInit(){
+    this.playAnimation(3600);
+  }
+
+  playAnimation(time:number){
+    this.typeAnimationh1 = true;    
+
+    setTimeout(() => {
+      this.typeAnimationh1 = false;
+      this.hideh3 = false;
+      this.tpyeAnimationh3 = true;
+      setTimeout(() => {
+        this.tpyeAnimationh3 = false;
+      }, time);
+    }, time);
+  }
 }
