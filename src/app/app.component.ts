395d86adfa1693ad9052aa86eb164cd6063bcf8b
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-12-13 14:43:40 +0100

    add an arrow section between components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/components/header/header.component";
 import { FooterComponent } from "./shared/components/footer/footer.component";
 import { BigPicGreetComponent } from "./big-pic-greet/big-pic-greet.component";
+import { ArrowComponent } from "./arrow/arrow.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, BigPicGreetComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, BigPicGreetComponent, ArrowComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/arrow/arrow.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-arrow',
+  standalone: true,
+  imports: [],
+  template: '<div class="arrow"></div>',
+  styleUrl: './arrow.component.scss'
+})
+export class ArrowComponent {
+
+}
