dfe261519f609d684fe4958a112b92ba4038678b
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-12-13 14:01:38 +0100

    fixed config for path i18n on server

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,7 +8,7 @@ import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 
 export function createTranslateLoader(http: HttpClient) {
-  return new TranslateHttpLoader(http, './angular-projects/myportfolio/assets/i18n/', '.json');
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
 
@@ -18,11 +18,12 @@ export const appConfig: ApplicationConfig = {
       TranslationService,
       importProvidersFrom(
         HttpClientModule,
+
         TranslateModule.forRoot({
           defaultLanguage: 'en',
           loader: {
             provide: TranslateLoader,
-            useFactory: (http: HttpClient) => new TranslateHttpLoader(http),
+            useFactory: (createTranslateLoader),
             deps: [HttpClient]
           }
         })
