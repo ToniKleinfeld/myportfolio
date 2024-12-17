70ccbc0ed310ecfe1109c340651a1b628e215822
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-12-17 15:39:20 +0100

    create skillset Component get some new svg icons in orginial colors add scss icon to list

--- src/app/skillset/skillset.component.ts
+++ src/app/skillset/skillset.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-skillset',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,TranslateModule],
   templateUrl: './skillset.component.html',
   styleUrl: './skillset.component.scss'
 })
 export class SkillsetComponent {
-
+ iconArray = ['Angular','TypeScript','JavaScript','Rest-Api','Firebase','Git','Scrum','SCSS','Material','HTML','CSS','Challenge']
 }
