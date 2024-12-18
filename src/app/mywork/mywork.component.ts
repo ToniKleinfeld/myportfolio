4e763ebefdbbbdd87b024748e4d611b80e99835b
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-12-18 17:36:26 +0100

    creating html with for array much scss styling not finish

--- src/app/mywork/mywork.component.ts
+++ src/app/mywork/mywork.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-mywork',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './mywork.component.html',
-  styleUrl: './mywork.component.scss'
+  styleUrl: './mywork.component.scss',
 })
 export class MyworkComponent {
-
+  projectsArray = [
+    { 
+      name: 'Join',
+      about: 'mywork.joinabout',
+      skills: 'Angular, JavaScript, Firebase, CSS, HTML, Scrum',
+      learned: 'mywork.joinlearned',
+    }
+  ];
 }
