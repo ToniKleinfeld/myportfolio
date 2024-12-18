a05d41a169a8e0f1d74d4fe32855071ff0150c3a
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-12-18 14:26:40 +0100

    rework sidescrolle

--- src/app/horizonalscroll.directive.ts
+++ src/app/horizonalscroll.directive.ts
@@ -1,19 +1,46 @@
-import { Directive, HostListener , ElementRef } from '@angular/core';
+import { Directive, HostListener, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appHorizonalscroll]',
   standalone: true
 })
-
 export class HorizonalscrollDirective {
+  private scrollTimeout: any;
+  private lastScrollTime = 0;
+  private smoothingFactor = 1; 
+  private maxScrollSpeed = 170; 
+
   constructor(private el: ElementRef) {}
 
   @HostListener('wheel', ['$event']) 
   onWheel(event: WheelEvent) {
     if (window.innerWidth > 1025) {
       event.preventDefault();
-      const scrollAmount = event.deltaY;      
-      this.el.nativeElement.scrollLeft += scrollAmount*4;
+      const currentTime = Date.now();
+      const rawScrollAmount = event.deltaY;
+      const clampedScrollAmount = this.limitScrollSpeed(rawScrollAmount);
+      const smoothScrollAmount = this.calculateSmoothScroll(clampedScrollAmount);
+      this.el.nativeElement.scrollLeft += smoothScrollAmount;
+
+      if (this.scrollTimeout) {
+        clearTimeout(this.scrollTimeout);
+      }
+
+      this.scrollTimeout = setTimeout(() => {
+        this.scrollTimeout = null;
+      }, 100);
+
+      this.lastScrollTime = currentTime;
     }
+    
+  }
+
+  private limitScrollSpeed(scrollAmount: number): number {
+    return Math.sign(scrollAmount) * 
+      Math.min(Math.abs(scrollAmount), this.maxScrollSpeed);
+  }
+
+  private calculateSmoothScroll(scrollAmount: number): number {
+    return scrollAmount * this.smoothingFactor * 4;
   }
 }
