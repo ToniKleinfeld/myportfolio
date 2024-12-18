289fa8fc7879bdd741d5a58ed76d2a83677d6956
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-12-18 15:05:39 +0100

    creating my Work Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,13 @@ import { BigPicGreetComponent } from "./big-pic-greet/big-pic-greet.component";
 import { ArrowComponent } from "./arrow/arrow.component";
 import { WhymeComponent } from "./whyme/whyme.component";
 import { SkillsetComponent } from "./skillset/skillset.component";
+import { MyworkComponent } from "./mywork/mywork.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, BigPicGreetComponent, ArrowComponent, WhymeComponent, HorizonalscrollDirective, SkillsetComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, BigPicGreetComponent, ArrowComponent, WhymeComponent, HorizonalscrollDirective, SkillsetComponent, MyworkComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
