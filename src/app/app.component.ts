a84008b008add6d48b506d5bc7f3e82b27569943
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2024-12-27 13:57:25 +0100

    remove arrow component add mixin for arrow and h2 to fix arrow on h2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { HorizonalscrollDirective } from './horizonalscroll.directive';
 import { HeaderComponent } from "./shared/components/header/header.component";
 import { FooterComponent } from "./shared/components/footer/footer.component";
 import { BigPicGreetComponent } from "./big-pic-greet/big-pic-greet.component";
-import { ArrowComponent } from "./arrow/arrow.component";
 import { WhymeComponent } from "./whyme/whyme.component";
 import { SkillsetComponent } from "./skillset/skillset.component";
 import { MyworkComponent } from "./mywork/mywork.component";
@@ -15,7 +14,7 @@ import { ReferencesComponent } from "./references/references.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, BigPicGreetComponent, ArrowComponent, WhymeComponent, HorizonalscrollDirective, SkillsetComponent, MyworkComponent, ReferencesComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, BigPicGreetComponent, WhymeComponent, HorizonalscrollDirective, SkillsetComponent, MyworkComponent, ReferencesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
