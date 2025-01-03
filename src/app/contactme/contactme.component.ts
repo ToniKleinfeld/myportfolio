141a76f751482d850f6008e118aaf28ce86a75c6
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-01-03 12:19:11 +0100

    add pattern modifided form add more labels

--- src/app/contactme/contactme.component.ts
+++ src/app/contactme/contactme.component.ts
@@ -1,13 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contactme',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule,FormsModule,CommonModule],
   templateUrl: './contactme.component.html',
   styleUrl: './contactme.component.scss'
 })
 export class ContactmeComponent {
+
+  formData= {
+    name : "",
+    email : "",
+    message : "",
+  
+  }
+
+  onSubmit(ngForm : NgForm){
+    if (ngForm.valid && ngForm.submitted) {
+      console.log(this.formData)
+    }    
+  }
+
+
 
 }
