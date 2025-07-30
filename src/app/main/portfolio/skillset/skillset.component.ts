e7293845a9ef13af0aafe9eed5806dcaf44a4bc0
Toni Kleinfeld <54000942+gaunerleechy@users.noreply.github.com>
2025-07-30 17:14:34 +0200

    Refactor skillset component to use iconArray object and integrate AddSpacePipe for icon names

--- src/app/main/portfolio/skillset/skillset.component.ts
+++ src/app/main/portfolio/skillset/skillset.component.ts
@@ -2,15 +2,49 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { ScrollAnimationDirective } from '../../../directives/scroll-animation.directive';
-
+import { AddSpacePipe } from '../../../shared/pipes/add-space.pipe';
 
 @Component({
   selector: 'app-skillset',
   standalone: true,
-  imports: [CommonModule,TranslateModule,ScrollAnimationDirective],
+  imports: [CommonModule, TranslateModule, ScrollAnimationDirective, AddSpacePipe],
   templateUrl: './skillset.component.html',
-  styleUrl: './skillset.component.scss'
+  styleUrl: './skillset.component.scss',
 })
 export class SkillsetComponent {
- iconArray = ['Angular','TypeScript','JavaScript','Firebase','Rest-Api','Git','Scrum','SCSS','CSS','HTML','Material','Bootstrap','Challenge']
+  frondendSkills: boolean = false;
+  // iconArray = ['Angular','TypeScript','JavaScript','Firebase','Rest-Api','Git','Scrum','SCSS','CSS','HTML','Material','Bootstrap','Challenge']
+  iconArray = {
+    frondend: [
+      'Angular',
+      'TypeScript',
+      'JavaScript',
+      'Firebase',
+      'Rest-Api',
+      'Git',
+      'Scrum',
+      'SCSS',
+      'CSS',
+      'HTML',
+      'Material',
+      'Bootstrap',
+      'Challenge',
+    ],
+    backend: [
+      'Django',
+      'Django_RF',
+      'Python',
+      'PostgreSQL',
+      'Linux',
+      'SQLite',
+      'Docker',
+      'Google_cloud',
+      'Github_Action',
+      'Challenge'
+    ],
+  };
+
+  setSkillSet() {
+    return this.frondendSkills ? this.iconArray.frondend : this.iconArray.backend;
+  }
 }
